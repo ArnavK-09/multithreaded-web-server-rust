dfd0f1cc6b4990c80ef4e46fc941f978345ecfb5
ArnavK-09 <arnavkaushal09@gmail.com>
2026-08-17 00:42:09 +0530

    Start TCP server and accept connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6967").unwrap();
+
+    print!("\x1B[2J\x1B[1;1H");
+    println!(
+        "\nListening on: http://{}\n",
+        listener.local_addr().unwrap()
+    );
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established...");
+        dbg!(stream);
+    }
 }
