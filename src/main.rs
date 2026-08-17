d6089592e68ad5a1eb0014c5e85f4c0ca9bff3bc
ArnavK-09 <arnavkaushal09@gmail.com>
2026-08-17 21:47:36 +0530

    Add thread pool to Rust web server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use multithreaded_web_server_rust::Threadpool;
 use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -12,20 +15,28 @@ fn main() {
         listener.local_addr().unwrap()
     );
 
+    let pool = Threadpool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream)
+        pool.execute(|| handle_connection(stream));
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let req = buf_reader.lines().next().unwrap().unwrap();
+    let req = match buf_reader.lines().next() {
+        Some(Ok(l)) => l,
+        _ => return,
+    };
 
-    let (status_line, filename) = if req == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &req[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let content = fs::read_to_string(filename).unwrap();
