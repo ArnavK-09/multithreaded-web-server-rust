a3b9e6010e503874fecf2c3d0f7bb51b9320e57f
ArnavK-09 <arnavkaushal09@gmail.com>
2026-08-17 21:10:05 +0530

    Serve HTML responses over TCP connections

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::net::TcpListener;
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6967").unwrap();
 
-    print!("\x1B[2J\x1B[1;1H");
     println!(
         "\nListening on: http://{}\n",
         listener.local_addr().unwrap()
@@ -11,8 +14,23 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established...");
-        dbg!(stream);
+        handle_connection(stream)
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let req = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if req == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let length = content.len();
+    let res = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+    stream.write_all(res.as_bytes()).unwrap();
 }
